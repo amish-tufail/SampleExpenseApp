331e96d89f75ac3b869887131a6e3d396a50051e
Amish <amish.tufail2002@gmail.com>
2023-11-25 11:39:06 +0500

    Initial Commit

---
+++ SampleWWTesting/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SampleWWTesting
+//
+//  Created by Amish Tufail on 25/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SampleWWTesting/SampleWWTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleWWTestingApp.swift
+//  SampleWWTesting
+//
+//  Created by Amish Tufail on 25/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SampleWWTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
